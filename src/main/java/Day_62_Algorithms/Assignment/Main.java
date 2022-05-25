d303a4630b23c01a27d1e36ab8b4cc82e2f600ab
404HoneyBadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-05-25 06:14:42 -0400

    first commit

---
+++ src/main/java/Day_62_Algorithms/Assignment/Main.java
@@ -0,0 +1,51 @@
+package Day_62_Algorithms.Assignment;
+
+import java.util.Arrays;
+
+public class Main {
+
+    void bubbleSort(int[] arr, int n) {
+        n= arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                }
+    }
+//
+static int partition(int[] arr, int low, int high) {
+    int pivot = arr[high];
+
+    int i = (low - 1);
+
+    for(int j = low; j <= high - 1; j++) {
+
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+
+    static void quickSort(int[] arr, int low, int high){
+        if (low < high){
+
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+}
+
+
+
